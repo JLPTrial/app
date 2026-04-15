2f54717df659e22b971988e648ec0656fbe80057
Otavio Capobianco <otaviogarciacapobianco@gmail.com>
2026-04-15 02:37:36 -0300

    feat: wait for pre-processing to finish before loading screens

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,21 @@
+import { moveDatabase } from "@/db/moveDatabase";
 import { Stack } from "expo-router";
+import { Paths } from 'expo-file-system';
+import { useEffect, useState } from "react";
+import Loading from "./loading";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    moveDatabase().then(() => setIsReady(true));
+  }, []);
+
+  if (!isReady) return <Loading />;
+
+  const n5Path = `${Paths.document.uri}/SQLite/N5.db`.replace('file://', '');
+
+  return (
+    <Stack />
+  );
 }
