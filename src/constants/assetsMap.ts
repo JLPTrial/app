aa7c94b2a6ff8aaa586524f9114a84fddb353c55
Otavio Capobianco <otaviogarciacapobianco@gmail.com>
2026-04-15 00:48:23 -0300

    fix: create script that maps static media to their requires

---
+++ src/constants/assetsMap.ts
@@ -0,0 +1,5 @@
+export const assetsMap: Record<string, any> = {
+  // This dictionary is generated on build
+  // Each of its lines is of the form:
+  // "'<JLPTLevel (N5, N4,...)><file path as listed on .db>': require('path/to/file')"
+};
