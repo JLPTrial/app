2e904e748518e7007891f216e134b4e753ba3786
João Fernandes <joaovictor.fernandes@usp.br>
2026-04-21 18:12:59 -0300

    feat: format audio time

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -4,6 +4,13 @@ import { useEffect } from 'react';
 import { Ionicons } from '@expo/vector-icons';
 import Slider from '@react-native-community/slider';
 
+function formatTime(seconds: number) : string {
+  const min = Math.floor(seconds / 60);
+  const sec = Math.floor(seconds % 60);
+
+  return `${min}:${sec.toString().padStart(2, '0')}`;
+}
+
 export default function AudioPlayer({ source }: { source: AudioSource }) {
   const player = useAudioPlayer(source);
   const status = useAudioPlayerStatus(player);
@@ -16,38 +23,38 @@ export default function AudioPlayer({ source }: { source: AudioSource }) {
   }, [status?.didJustFinish]);
 
   return (
-      <View style={{ flexDirection: 'row', alignItems: 'center'}}>
-        <Pressable
-        onPress={() => {
-          if (status.playing) {
-            player.pause();
-          } else {
-            player.play();
-          }
-        }}
-      >
-        <Ionicons
-          name={status.playing ? 'pause' : 'play'}
-          size={32}
-          color="#000"
-        />
-      </Pressable>
-
-      <Slider
-        style={{width: 200, height: 40}}
-        value={status.currentTime}
-        onSlidingComplete={(value) => {
-                player.seekTo(value);
-            }
+  <View style={{ flexDirection: 'row', alignItems: 'center'}}>
+    <Pressable
+      onPress={() => {
+        if (status.playing) {
+          player.pause();
+        } else {
+          player.play();
         }
-        minimumValue={0}
-        maximumValue={status.duration}
-        minimumTrackTintColor="#000000"
-        maximumTrackTintColor="#000000"
-        thumbTintColor="#000000"
-        thumbSize={32}
+      }}
+    >
+      <Ionicons
+        name={status.playing ? 'pause' : 'play'}
+        size={32}
+        color="#000"
       />
-      <Text>{Math.floor(status.currentTime)}s/{Math.floor(status.duration)}s</Text>
-    </View>
+    </Pressable>
+
+    <Slider
+      style={{width: 200, height: 40}}
+      value={status.currentTime}
+      onSlidingComplete={(value : number) => {
+              player.seekTo(value);
+          }
+      }
+      minimumValue={0}
+      maximumValue={status.duration}
+      minimumTrackTintColor="#000000"
+      maximumTrackTintColor="#000000"
+      thumbTintColor="#000000"
+      thumbSize={32}
+    />
+    <Text>{formatTime(status.currentTime)}/{formatTime(status.duration)}</Text>
+  </View>
   );
 }
