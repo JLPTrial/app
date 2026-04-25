3084bbd42fd66a41becca08fedbf56e990b88efc
João Fernandes <joaovictor.fernandes@usp.br>
2026-04-24 23:23:45 -0300

    feat: add praticte-test page

---
+++ src/app/practice-test.tsx
@@ -0,0 +1,12 @@
+import Screen from '@/components/Screen';
+import { Text } from 'react-native';
+
+export default function PracticeTest() {
+
+  return (
+    <Screen>
+      <Text>Simulado!!!</Text>
+    </Screen>
+    
+  );
+}
