3d4009d2bb2a0cfe10e6041b316fa42eda5d549d
João Fernandes <joaovictor.fernandes@usp.br>
2026-04-30 18:04:16 -0300

    feat: improve component styles

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -1,19 +1,23 @@
 import { colors } from '@/styles/globals';
 import React, { PropsWithChildren } from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, StyleProp, ViewStyle } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function Screen({ children } : PropsWithChildren){
+type ScreenProps = PropsWithChildren<{
+    style?: StyleProp<ViewStyle>;
+}>;
+
+export default function Screen({ children, style } : ScreenProps){
 
     return (
         <SafeAreaView style={{flex: 1, backgroundColor: colors.background }}>
-            <ScrollView contentContainerStyle={{ 
-                justifyContent: 'center',
+            <ScrollView contentContainerStyle={[{ 
+                flexGrow: 1,
                 alignItems: 'center',
                 paddingHorizontal: 24,
                 gap: 20,
                 
-                }}>
+                }, style]}>
 
                 {children}
 
