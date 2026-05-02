3cdeb211c486a808a2fe16efa729fecbbc03fcf8
João <joao.bcc@usp.br>
2026-05-02 01:43:48 -0300

    chore: update AudioPlayer.tsx styles

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -1,10 +1,11 @@
-import { StyleSheet, Pressable, Text, View } from 'react-native';
-import { AudioSource, useAudioPlayer, useAudioPlayerStatus } from 'expo-audio';
-import { useEffect, useState } from 'react';
 import { Ionicons } from '@expo/vector-icons';
 import Slider from '@react-native-community/slider';
+import { AudioSource, useAudioPlayer, useAudioPlayerStatus } from 'expo-audio';
+import { useEffect, useState } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { colors } from '../styles/globals';
 
-function formatTime(seconds: number) : string {
+function formatTime(seconds: number): string {
   const min = Math.floor(seconds / 60);
   const sec = Math.floor(seconds % 60);
 
@@ -26,65 +27,66 @@ export default function AudioPlayer({ source }: { source: AudioSource }) {
   }, [status?.didJustFinish]);
 
   return (
-  <View style={styles.container}>
-    <Pressable
-      onPress={() => {
-        if (status.playing) {
-          player.pause();
-        } else {
-          player.play();
-        }
-      }}
-    >
-      <Ionicons
-        name={status.playing ? 'pause' : 'play'}
-        size={32}
-        color="#000"
-      />
-    </Pressable>
+    <View style={styles.container}>
+      <Pressable
+        onPress={() => {
+          if (status.playing) {
+            player.pause();
+          } else {
+            player.play();
+          }
+        }}
+      >
+        <Ionicons
+          name={status.playing ? 'pause' : 'play'}
+          size={32}
+          color={colors.textDark}
+        />
+      </Pressable>
 
-    <Slider
-      style={styles.slider}
-      value={status.currentTime}
-      onSlidingStart={(value : number) => {
+      <Slider
+        style={styles.slider}
+        value={status.currentTime}
+        onSlidingStart={(value: number) => {
           setIsSliding(true);
           player.pause();
         }
-      }
-      onValueChange={(value : number) => {
+        }
+        onValueChange={(value: number) => {
           setSliderTime(value);
         }
-      }
-      onSlidingComplete={(value : number) => {
-            setIsSliding(false);
-            player.seekTo(value);
-            player.play();
-          }
-      }
-      minimumValue={0}
-      maximumValue={status.duration}
-      minimumTrackTintColor="#000000"
-      maximumTrackTintColor="#000000"
-      thumbTintColor="#000000"
-    />
-    <Text>{formatTime(isSliding ? sliderTime : status.currentTime)}/{formatTime(status.duration)}</Text>
-  </View>
+        }
+        onSlidingComplete={(value: number) => {
+          setIsSliding(false);
+          player.seekTo(value);
+          player.play();
+        }
+        }
+        minimumValue={0}
+        maximumValue={status.duration}
+        minimumTrackTintColor={colors.textDark}
+        maximumTrackTintColor={colors.textDark}
+        thumbTintColor={colors.textDark}
+      />
+
+      <Text>{formatTime(isSliding ? sliderTime : status.currentTime)}/{formatTime(status.duration)}</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container : {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 10,
-        borderWidth: 0,
-        backgroundColor: '#ddd',
-        borderRadius: 30,
-        maxWidth: '90%',
-    },
-    slider : {
-      flex: 1,
-      height: 30,
-    }
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    borderWidth: 0,
+    backgroundColor: colors.audioPlayer,
+    borderRadius: 30,
+    maxWidth: '90%',
+  },
+  slider: {
+    flex: 1,
+    height: 30,
+  }
 });
