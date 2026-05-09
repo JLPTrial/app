4a32a10017d449d3f317ace6aafa2065ecf14603
João Fernandes <joaovictor.fernandes@usp.br>
2026-05-08 22:45:07 -0300

    feat: add parser tests to topic study screen

--- src/app/topic-study.tsx
+++ src/app/topic-study.tsx
@@ -1,11 +1,23 @@
 import Screen from '@/components/Screen';
-import { Text } from 'react-native';
+import parser from '@/utils/parsers';
+
+const examples = [
+  "もう　（[blank]）　を　おくりました　よ。",
+  "[underline_blank]　の　ことばは　ひらがなで　どう　かきますか。",
+  "ヤンさん　は　この　つくえ　を[underline_blank][underline_blank][star_underline_blank][underline_blank]よ。",
+  "いつも　ここ　で　{しんぶん}　を　かいます。",
+  "おふろ　に　入[はい]って　すこし　休[やす]んで　から、べんきょうします。"
+];
 
 export default function Exam() {
 
   return (
     <Screen>
-      <Text>Estude por tópicos!!!</Text>
+      {parser(examples[0])}
+      {parser(examples[1])}
+      {parser(examples[2])}
+      {parser(examples[3])}
+      {parser(examples[4])}
     </Screen>
     
   );
