3776381c3c14e7bb692cc260de37cd203107a537
João <joao.bcc@usp.br>
2026-05-23 00:46:33 -0300

    feat: create questions session end screen

---
+++ src/components/EndScreen.tsx
@@ -0,0 +1,85 @@
+import PercentageBar from '@/components/PercentageBar';
+import Screen from '@/components/Screen';
+import { AppText } from '@/components/texts/AppText';
+import { colors } from '@/styles/globals';
+import { router } from 'expo-router';
+import { Pressable, StyleSheet, View } from 'react-native';
+
+type SessionResult = {
+    right: number;
+    total: number;
+};
+
+export default function EndScreen({ result }: { result: SessionResult }) {
+  const percentage = result.total > 0 ? Math.round((result.right / result.total) * 100) : 0;
+  const scoreColor =
+        percentage >= 70 ? colors.success :
+          percentage >= 50 ? colors.mid :
+            colors.failure;
+  const feedbackText =
+        percentage >= 90 ? 'おめでとう! Desempenho excelente.' :
+          percentage >= 70 ? 'Ótimo desempenho!' :
+            percentage >= 50 ? 'Boa tentativa!' :
+              'Lembre-se de sempre revisar os seus erros!';
+
+  return (
+    <Screen>
+      <AppText variant='title'>
+                Sessão Concluída!
+      </AppText>
+
+      <View style={styles.resultCard}>
+        <AppText style={{ color: colors.textMuted }}>Questões corretas</AppText>
+        <AppText bold={true} style={[styles.scoreText, { color: scoreColor }]}>
+          {result.right}/{result.total}
+        </AppText>
+        <AppText style={[styles.percentageText, { color: scoreColor }]}>
+          {percentage}%
+        </AppText>
+
+        <PercentageBar
+          progress={percentage}
+          color={scoreColor}
+        />
+      </View>
+
+      <AppText center={true}>
+        {feedbackText}
+      </AppText>
+
+      <View style={styles.footer}>
+        <Pressable style={styles.homeButton} onPress={() => router.dismissAll()}>
+          <AppText bold={true} variant='title' style={{ color: colors.textLight }}>
+                        Voltar ao Início
+          </AppText>
+        </Pressable>
+      </View>
+    </Screen>);
+}
+const styles = StyleSheet.create({
+  resultCard: {
+    backgroundColor: colors.successBlock,
+    borderRadius: 16,
+    padding: 24,
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    gap: 8,
+  },
+  scoreText: {
+    fontSize: 48,
+  },
+  percentageText: {
+    fontSize: 24,
+  },
+  footer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignSelf: 'stretch',
+  },
+  homeButton: {
+    backgroundColor: colors.primary,
+    borderRadius: 16,
+    paddingVertical: 16,
+    alignItems: 'center',
+  },
+});
