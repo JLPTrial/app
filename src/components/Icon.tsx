12c201158e08a3062e442613b4ffdb82cd4a643f
Lucas Rabaquim <lucas.rabaquim@usp.br>
2026-06-05 15:36:29 -0300

    feat: add more flexible icon component
    O Ionicons não tem um icone de furigana e não tem ícone de
    vibração. FontAwesome só adicionou um ícone de vibração gratuito
    na V7, e não botaram o ícone de vibração cortado. O componente
    de icone serve para ter uma interface comum para icones
    personalizados.

---
+++ src/components/Icon.tsx
@@ -0,0 +1,36 @@
+import { StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { AppText } from "./texts/AppText";
+import { WithLocalSvg } from 'react-native-svg/css';
+
+type icon = keyof typeof Ionicons.glyphMap;
+
+const assets: Record<string, any> = {
+  //"name": require("../../assets/icons/name.svg"),
+}
+
+type iconProps = {
+  name : string, 
+  size : number, 
+  color : any
+};
+
+export const Icon = ({name, size, color} : iconProps ) => {
+  if(name in Ionicons.glyphMap){
+    return <Ionicons
+        name={name as icon}
+        size={size}
+        color={color}
+      />;
+  }
+  if(name in assets){
+    return <WithLocalSvg
+          asset={assets[name]}
+          width={size}
+          height={size}
+          fill={color}
+          color={color}
+        />
+  }
+  return <AppText>{name}</AppText>
+}
