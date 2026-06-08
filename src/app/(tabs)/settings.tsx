24de25f9a8ae5057251c546b4ed640d771d46492
Lucas Rabaquim <lucas.rabaquim@usp.br>
2026-06-08 18:23:22 -0300

    feat: save preferences on StorageContext

--- src/app/(tabs)/settings.tsx
+++ src/app/(tabs)/settings.tsx
@@ -2,29 +2,41 @@ import { AppText } from '@/components/texts/AppText';
 import { useState } from 'react';
 import { ScrollView, StyleSheet, View } from 'react-native';
 import { SwitchSetting, SliderSetting, ActionSetting, SettingCard } from '../../components/Settings';
+import { useStorage } from '@/hooks/useStorage';
+import { colors } from '@/styles/globals';
 
 export default function SettingsScreen() {
-  const [isDarkMode, setDarkMode] = useState(false);
-  const [isFuriganaOn, setFurigana] = useState(false);
+  const {data, setValue} = useStorage();
 
-  const [isHapticFeedbackOn, setHapticFeedback] = useState(false);
+  const [isDarkMode, setDarkMode] = useState(data.darkMode);
+  const [isFuriganaOn, setFurigana] = useState(data.furigana);
 
-  const [fontSize, setFontSize] = useState(0);
-  const [volume, setVolume] = useState(0);
+  const [isHapticFeedbackOn, setHapticFeedback] = useState(data.hapticFeedback);
+
+  const [fontSize, setFontSize] = useState(data.fontSize);
+  const [volume, setVolume] = useState(data.volume);
 
   return (
     <ScrollView>
       <View style={styles.container}>
-        <AppText>Configurações</AppText>
+        <AppText bold style={styles.header}>Configurações</AppText>
 
         <SettingCard title='Aparência'>
-          <SwitchSetting icon={isDarkMode ? 'moon' : 'sunny'} title='Modo' onChange={setDarkMode} value={isDarkMode} />
+          <SwitchSetting 
+            icon={isDarkMode ? 'moon' : 'sunny'} 
+            title='Modo' 
+            onChange={(value) => {setDarkMode(value); setValue('darkMode', value); }} 
+            value={isDarkMode} />
 
-          <SwitchSetting icon='振' title='Furigana' onChange={setFurigana} value={isFuriganaOn} />
+          <SwitchSetting 
+            icon='振' 
+            title='Furigana' 
+            onChange={(value) => {setFurigana(value); setValue('furigana', value); }}
+            value={isFuriganaOn} />
 
           <SliderSetting
             title='Fonte'
-            onChange={(value) => setFontSize(value)}
+            onChange={(value) => {setFontSize(value); setValue('fontSize', value);}}
             value={fontSize}
             min={0}
             step={1}
@@ -35,13 +47,17 @@ export default function SettingsScreen() {
           />
         </SettingCard>
 
-        <SettingCard title='Som'>
-          <SwitchSetting icon='ellipse' title='Feedback tátil' onChange={setHapticFeedback} value={isHapticFeedbackOn} />
+        <SettingCard title='Interação'>
+          <SwitchSetting 
+            icon='ellipse' 
+            title='Feedback tátil' 
+            onChange={(value) => {setHapticFeedback(value); setValue('hapticFeedback', value); }}
+            value={isHapticFeedbackOn} />
 
           <SliderSetting
             title='Volume Interno'
             value={volume}
-            onChange={(value) => setVolume(value)}
+            onChange={(value) => {setVolume(value); setValue('volume', value);}}
             min={0}
             max={100}
           />
@@ -63,5 +79,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'space-around',
     alignItems: 'center',
+  },
+  header:{
+    backgroundColor:colors.primary,
+    color: colors.background,
+    padding:10,
+    borderRadius:999,
+    margin:10,
   },
 });
