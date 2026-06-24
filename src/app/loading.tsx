682bb8b554f482cdf2b840086eb43fd1fff51eb5
johntruck <joaodobrasilsegundo@gmail.com>
2026-06-24 18:00:14 -0300

    feat: make loading screen be able to get colors before StorageProvider is called

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,15 +1,32 @@
+import { darkColors, lightColors } from '@/styles/globals';
 import { Image } from 'expo-image';
-import { StyleSheet, Text, View } from 'react-native';
+import { Storage } from 'expo-sqlite/kv-store';
+import { Appearance, StyleSheet, Text, View } from 'react-native';
+
+// A tela de loading pode ser chamada antes do StorageProvider ser chamado
+function getColorsBeforeContext() {
+  try {
+    const stored = Storage.getItemSync('theme');
+    if (stored) {
+      const parsed = JSON.parse(stored);
+      if (parsed === 'dark') return darkColors;
+      if (parsed === 'light') return lightColors;
+    }
+  } catch {}
+  return Appearance.getColorScheme() === 'dark' ? darkColors : lightColors;
+}
 
 export default function Loading() {
+  const colors = getColorsBeforeContext();
+
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { backgroundColor: colors.background }]}>
 	  <Image
         source={require('@/assets/images/logo.jpg')}
         style={styles.logo}
         contentFit="contain"
 	  />
-	  <Text style={styles.message}>
+	  <Text style={[styles.message, { color: colors.textDark }]}>
         {'Carregando...'}
 	  </Text>
     </View>
