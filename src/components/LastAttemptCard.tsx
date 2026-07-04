edac816c9ed24b6cb64236e0f394bae3ee1309dc
João Fernandes <joaovictor.fernandes@usp.br>
2026-07-03 22:05:41 -0300

    feat: create LastAttemptCard

--- src/components/LastAttemptCard.tsx
+++ src/components/LastAttemptCard.tsx
@@ -1,11 +1,35 @@
-import { Text, View } from "react-native";
+import { useQuestions } from "@/db/queries";
+import { useStorage } from "@/hooks/useStorage";
+import { ExamAttempt } from "@/types/types";
+import { useEffect, useState } from "react";
+import { View } from "react-native";
+import ResultCard from "./ResultCard";
+import { AppText } from "./texts/AppText";
 
 
 export default function LastAttemptCard(){
+    const { data } = useStorage();
+    const level = data.jlptLevel;
+    const db = useQuestions(level);
+    const [lastExam, setLastExam] = useState<ExamAttempt | null>(null);
+
+    useEffect(() => {
+        const loadLastExam = async () => {
+        const exam = await db.selectLastExam();
+        setLastExam(exam);
+        };
+
+        loadLastExam();
+    }, []);
+
+    if (!lastExam) return <AppText>Nenhum simulado realizado.</AppText>;
+    
+    const date = new Date(Number(lastExam.started_at)).toLocaleString();
 
     return (
         <View>
-            <Text>Dados da Última Tentativa</Text>
+            <AppText>Dados da Última Tentativa:</AppText>
+            <ResultCard label={date} right={lastExam.correct_answers} total={lastExam.total_questions}/>
         </View>
     );
 }
