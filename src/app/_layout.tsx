577dec989d0c8d5533be1d0425cc89f28f6ea41d
João Fernandes <joaovictor.fernandes@usp.br>
2026-07-03 21:59:11 -0300

    feat: create exam tables

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -16,6 +16,48 @@ export default function RootLayout() {
 
   if (!isReady) return <Loading />;
 
+    const examDBCreation = `
+      CREATE TABLE IF NOT EXISTS exam_attempts (
+        id INTEGER PRIMARY KEY,
+
+        score INTEGER NOT NULL,
+
+        correct_answers INTEGER NOT NULL,
+
+        total_questions INTEGER NOT NULL,
+
+        started_at TEXT NOT NULL,
+
+        finished_at TEXT,
+
+        approved INTEGER NOT NULL CHECK (approved IN (0, 1)),
+
+        jlpt_level TEXT NOT NULL
+      );
+
+      CREATE TABLE IF NOT EXISTS exam_attempt_questions (
+        attempt_id INTEGER NOT NULL,
+
+        question_id INTEGER NOT NULL,
+
+        question_order INTEGER NOT NULL,
+
+        selected_alternative INTEGER,
+
+        is_correct INTEGER NOT NULL CHECK (is_correct IN (0, 1)),
+
+        PRIMARY KEY (attempt_id, question_order),
+
+        FOREIGN KEY (attempt_id)
+          REFERENCES exam_attempts(id)
+          ON DELETE CASCADE,
+
+        FOREIGN KEY (question_id)
+          REFERENCES questions(id)
+          ON DELETE CASCADE
+      );
+    `
+
   return (
     <Suspense fallback={<Loading />}>
       <SQLiteProvider
@@ -24,6 +66,7 @@ export default function RootLayout() {
           await db.execAsync("PRAGMA foreign_keys = ON;");
           attachQuestionsDatabase(db);
           createAnswerTable(db);
+          await db.execAsync(examDBCreation);
         }}
       >
         <StorageProvider>
