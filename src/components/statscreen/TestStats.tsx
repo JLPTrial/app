c4d1ef7d703477e4c48813b4d7b5b5cc74a01b65
Lucas Rabaquim <lucas.rabaquim@usp.br>
2026-07-20 14:04:56 -0300

    feat: add tabs to stats screen

---
+++ src/components/statscreen/TestStats.tsx
@@ -0,0 +1,10 @@
+import Screen from '@/components/Screen';
+import { AppText } from '../texts/AppText';
+
+export default function TestStats() {
+  return (
+    <Screen internal>
+      <AppText>Simulado</AppText>
+    </Screen>
+  );
+}
