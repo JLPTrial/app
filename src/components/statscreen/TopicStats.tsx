fe3ebabfdfe30c21fcc84b99517428f89906672d
Lucas Rabaquim <lucas.rabaquim@usp.br>
2026-07-20 14:04:56 -0300

    feat: add tabs to stats screen

---
+++ src/components/statscreen/TopicStats.tsx
@@ -0,0 +1,10 @@
+import Screen from '@/components/Screen';
+import { AppText } from '../texts/AppText';
+
+export default function TopicStats() {
+  return (
+    <Screen internal>
+      <AppText>Tópicos</AppText>
+    </Screen>
+  );
+}
