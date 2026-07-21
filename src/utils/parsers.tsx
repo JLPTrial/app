6b263c1bd543fb5ebb670a0a0c9f2db4cf24a674
Otavio Capobianco <otaviogarciacapobianco@gmail.com>
2026-07-21 20:29:37 -0300

    style: remove blank lines between parser's regex clauses

--- src/utils/parsers.tsx
+++ src/utils/parsers.tsx
@@ -2,19 +2,12 @@ export default function statementParser(statement: string): string[] {
   const regex = new RegExp(
     [
       String.raw`\[(?:blank|underline_blank|star_underline_blank)\]`,
-
       String.raw`\{[^}]+\}`,
-
       String.raw`\p{Script=Han}+\[[^\]]+\]`,
-
       String.raw`\p{Script=Hiragana}+`,
-
       String.raw`\p{Script=Katakana}+`,
-
       "[、。「」『』（）！？：；]",
-
       String.raw`\s+`,
-
       "."
     ].join("|"),
     "gu"
