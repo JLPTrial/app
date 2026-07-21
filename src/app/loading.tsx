bd1b0725fdbc983b4c932ce8711262d4ce9b0a17
johntruck <joaodobrasilsegundo@gmail.com>
2026-07-21 06:59:32 -0300

    feat: replace loading text with loading ball

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,21 +1,20 @@
+import Screen from '@/components/Screen';
 import { useDisplayColors } from '@/components/ThemeTransition';
 import { Image } from 'expo-image';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet } from 'react-native';
 
 export default function Loading() {
   const colors = useDisplayColors();
 
   return (
-    <View style={[styles.container, { backgroundColor: colors.background }]}>
-	  <Image
+    <Screen style={styles.container}>
+      <Image
         source={require('@/assets/images/logo.jpg')}
         style={styles.logo}
         contentFit="contain"
-	  />
-	  <Text style={[styles.message, { color: colors.textDark }]}>
-        {'Carregando...'}
-	  </Text>
-    </View>
+      />
+      <ActivityIndicator size="large" color={colors.primary} />
+    </Screen>
   );
 }
 
@@ -30,8 +29,5 @@ const styles = StyleSheet.create({
   logo: {
     width: 180,
     height: 180,
-  },
-  message: {
-    textAlign: 'center',
   },
 });
