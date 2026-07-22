c039829e479193612bd10849535e01f9d68d14ad
Lucas Rabaquim <lucas.rabaquim@usp.br>
2026-07-22 16:25:58 -0300

    feat: add jlpt level to stat screen

--- src/components/statscreen/TestStats.tsx
+++ src/components/statscreen/TestStats.tsx
@@ -1,10 +1,14 @@
 import Screen from '@/components/Screen';
 import { AppText } from '../texts/AppText';
+import { useStorage } from '@/hooks/useStorage';
+
 
 export default function TestStats() {
+  const { data } = useStorage();
+  const level = data.jlptLevel;
   return (
     <Screen internal>
-      <AppText>Simulado</AppText>
+      <AppText>Simulado do {level}</AppText>
     </Screen>
   );
 }
