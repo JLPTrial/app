10a4de9a0bbacddd700dc415323932327a08150f
Lucas Rabaquim <lucas.rabaquim@usp.br>
2026-07-22 18:24:57 -0300

    feat: simplify LineGraph component
    Vimos em aula alguma coisinha relacionada a "feat especulativa"...
    Removido o suporte a dois gráficos de linha para ficar mais
    fácil de usar a componente

--- src/components/graphs/AppLineGraph.tsx
+++ src/components/graphs/AppLineGraph.tsx
@@ -1,43 +1,35 @@
 import { LineChart } from "react-native-gifted-charts";
 import { colors, vw } from '@/styles/globals';
-import { useState } from "react";
 
 type AppLineGraphProps = {
-  values1: number[],
-  xAxisLabelTexts: string[],
-  onFocus?: (item: number, index: number) => void,
-  values2?: number[],
-  yAxisLabelTexts?: string[],
-  yAxisLabelSuffix?: string,
+  values: number[],
+  xLabels: string[],
+  maxValue: number,
+  yLabels?: string[],
+  yLabelsSulfix?: string,
+  onBackgroundPress?: () => void,
 }
 
-const none = () => {};
+export default function AppLineGraph({ values, xLabels, maxValue = 100, yLabels, yLabelsSulfix = "%", onBackgroundPress}: AppLineGraphProps) {
+  const data = values.map((value) => { return { value: value }; });
 
-export default function AppLineGraph({ values1, xAxisLabelTexts, onFocus = none, values2 = [], yAxisLabelTexts, yAxisLabelSuffix = "%" }: AppLineGraphProps) {
-  const [showData1, setShowData1] = useState<boolean>(true);
-  const data1 = values1.map((value) => { return { value: value }; });
-  const data2 = values2.map((value) => { return { value: value }; });
   return <LineChart
-    data={data1}
-    data2={data2}
-    xAxisLabelTexts={xAxisLabelTexts}
-    color1={showData1 ? colors.primaryLight : colors.textMuted}
-    color2={showData1 ? colors.textMuted : colors.primaryLight}
+    data={data}
+    xAxisLabelTexts={xLabels}
+    yAxisLabelTexts={yLabels}
+    yAxisLabelSuffix={yLabelsSulfix}
+    maxValue={maxValue}
+    onBackgroundPress={onBackgroundPress}
+    color={colors.primaryLight}
     dataPointsColor={colors.primary}
     sectionColors={Array(6).fill([colors.backgroundDim,colors.background]).flat()}
-    dataPointsWidth1={10}
+    dataPointsWidth={10}
     thickness={3}
-    onBackgroundPress={() => { if(values2.length > 0) setShowData1((value) => !value);}}
-    focusEnabled={onFocus !== none}
     dataPointsRadius={5}
-    focusProximity={300}
-    onFocus={(item: number, index: number) => onFocus(item, index)}
-    isAnimated={true}
-    maxValue={100}
     width={80 * vw}
-    adjustToWidth={true}
-    disableScroll={true}
-    yAxisLabelTexts={yAxisLabelTexts}
-    yAxisLabelSuffix={yAxisLabelSuffix}
+    animateOnDataChange
+    isAnimated
+    adjustToWidth
+    disableScroll
   />;
 }
