3ad368c3ea6e41f42215e65b34310bc5f35734ce
johntruck <joaodobrasilsegundo@gmail.com>
2026-07-27 04:03:52 -0300

    feat: add haptic feedback to TimerToggle

--- src/components/pressable/TimerToggle.tsx
+++ src/components/pressable/TimerToggle.tsx
@@ -1,3 +1,4 @@
+import { useHaptics } from "@/hooks/useHaptics";
 import { useStorage } from "@/hooks/useStorage";
 import { View } from "react-native";
 import { AppText } from "../texts/AppText";
@@ -5,6 +6,7 @@ import { AppSwitch } from "./AppSwitch";
 
 export default function TimerToggle() {
   const { data, setValue } = useStorage();
+  const haptics = useHaptics();
 
   return (
     <View
@@ -22,7 +24,10 @@ export default function TimerToggle() {
 
       <AppSwitch
         value={!!data.timer}
-        onChange={(value) => setValue("timer", value)}
+        onChange={(value) => {
+          haptics.selection();
+          setValue("timer", value);
+        }}
       />
 
     </View>
