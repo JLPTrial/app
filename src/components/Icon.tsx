2226c44a7220608aa1f6e856a28ad9ff17790d28
johntruck <joaodobrasilsegundo@gmail.com>
2026-07-27 01:37:26 -0300

    fix: let furigana icon size be fixed

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,9 +1,9 @@
 import { Ionicons } from '@expo/vector-icons';
-import { AppText } from "./texts/AppText";
-import { WithLocalSvg } from 'react-native-svg/css';
-import Statement from './texts/Statement';
+import { IconProps, TextAaIcon, VibrateIcon } from 'phosphor-react-native';
 import { View } from 'react-native';
-import { VibrateIcon, TextAaIcon, IconProps } from 'phosphor-react-native';
+import { WithLocalSvg } from 'react-native-svg/css';
+import { AppText } from "./texts/AppText";
+import Statement from './texts/Statement';
 
 type ionicon = keyof typeof Ionicons.glyphMap;
 
@@ -44,9 +44,15 @@ export const Icon = ({ name, size, color, furigana = '' }: iconProps) => {
     const Icon = phosphorIcons[name];
     return <Icon size={size} color={color} />;
   }
+
+  const furiganaDefaultSize = 0.7;
   if (name === 'furigana') {
-    return <View style={{ width: size, height: size }}>
-      <Statement statement={furigana} style={{ fontSize: size * 0.9, color: color }} />
+    return <View style={{ width: size, alignItems: 'center', justifyContent: 'center' }}>
+      <Statement
+        statement={furigana}
+        scaleWithFontSize={false}
+        style={{ fontSize: size * furiganaDefaultSize, color: color }}
+      />
     </View>;
   }
   return <AppText style={{ fontSize: size, color: color }}>{name}</AppText>;
