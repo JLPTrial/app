1322bb2587b51ae11e34941895be863b9ff68363
Otavio Capobianco <otaviogarciacapobianco@gmail.com>
2026-07-29 16:39:17 -0300

    fix: remove timer from topic study

--- src/app/session-lobby.tsx
+++ src/app/session-lobby.tsx
@@ -1,7 +1,6 @@
 import Loading from '@/app/loading';
 import LevelBadge from '@/components/containers/LevelBadge';
 import BottomButton from '@/components/pressable/BottomButton';
-import TimerToggle from '@/components/pressable/TimerToggle';
 import Screen from '@/components/Screen';
 import { AppText } from '@/components/texts/AppText';
 import { useQuestions } from '@/db/queries';
@@ -148,10 +147,6 @@ export default function SessionLobby() {
           </View>
         )
       }
-
-      <View style={{alignSelf: 'flex-start'}}>
-        <TimerToggle/>
-      </View>
 
       <BottomButton onPress={startSession} text="Iniciar" />
     </Screen >
